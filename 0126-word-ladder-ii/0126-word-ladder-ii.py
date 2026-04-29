23e9adfb7a9ec0a82282c7600c6c6c574f69540b
Sai Varshini <dargasaivarshini@gmail.com>
2026-04-29 11:23:45 +0530

    Time: 7 ms (96.21%), Space: 21 MB (5.08%) - LeetHub

---
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -0,0 +1,54 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def findLadders(self, bw, ew, wl):
+        if ew not in wl:
+            return []
+
+        wl.append(bw)
+
+        # Build adjacency (pattern -> words)
+        adj = defaultdict(list)
+        for word in wl:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                adj[pattern].append(word)
+
+        # BFS to build level map
+        queue = deque([bw])
+        level_map = {bw: 1}
+
+        while queue:
+            cur = queue.popleft()
+            step = level_map[cur]
+
+            if cur == ew:
+                break
+
+            for i in range(len(cur)):
+                pattern = cur[:i] + "*" + cur[i+1:]
+                for ne in adj[pattern]:
+                    if ne not in level_map:
+                        level_map[ne] = step + 1
+                        queue.append(ne)
+
+        res = []
+
+        # DFS to build paths
+        def dfs(cur, path):
+            if cur == bw:
+                res.append(path[::-1])
+                return
+
+            step = level_map[cur]
+
+            for i in range(len(cur)):
+                pattern = cur[:i] + "*" + cur[i+1:]
+                for ne in adj[pattern]:
+                    if level_map.get(ne, 0) == step - 1:
+                        dfs(ne, path + [ne])
+
+        if ew in level_map:
+            dfs(ew, [ew])
+        res = [list(p) for p in set(tuple(p) for p in res)]
+        return res
