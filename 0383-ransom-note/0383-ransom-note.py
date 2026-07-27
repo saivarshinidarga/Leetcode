aaefb244d7756cc0c385514f1ebfcee201fde90b
Sai Varshini <dargasaivarshini@gmail.com>
2026-07-27 23:15:48 +0530

    Delete 0383-ransom-note directory

--- 0383-ransom-note/0383-ransom-note.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for ch in ransomNote:
-            if ch not in magazine:
-                return False
-            magazine = magazine.replace(ch, "", 1)
-
-        return True
-        
